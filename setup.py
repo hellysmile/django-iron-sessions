c0c91fdc77c965afe0b8248aeb1e8976b5b49f95
hellysmile <hellysmile@gmail.com>
2013-03-01 15:00:34 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ def long_description():
 
 setup(
     name='django-iron-sessions',
-    version='0.0.1',
+    version='0.0.2',
     packages=packages,
     description=description,
     long_description=long_description(),
